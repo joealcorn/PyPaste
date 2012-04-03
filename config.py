5f3885ed5dff1ff0e48ce3918aefddeff9970689
Joe Alcorn <me@buttscicl.es>
2012-04-03 18:04:00 +0100

    I accidentally the config

---
+++ config.py
@@ -0,0 +1,4 @@
+DEBUG = False
+SQLALCHEMY_DATABASE_URI = 'sqlite:///pastebin.db' # http://docs.sqlalchemy.org/en/latest/dialects/sqlite.html#connect-strings
+SECRET_KEY = 'KEY'
+EMAIL = 'webmaster@site.com'
