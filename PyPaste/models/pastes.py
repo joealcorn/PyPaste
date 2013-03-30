b3004553763f4e1c3317d47e5db6a86bbe892d22
Joe Alcorn <me@buttscicl.es>
2013-03-30 21:24:49 +0000

    rewrite: Don't encrypt password protected pastes.
    It over complicates things (like viewing those pastes multiple times, do we store the plain text password somewhere?) and frankly it seems like overkill for a project that's supposed to be simple.

--- PyPaste/models/pastes.py
+++ PyPaste/models/pastes.py
@@ -1,10 +1,7 @@
 from os import urandom
 from datetime import datetime
-from hashlib import sha256, md5
-from base64 import b64encode, b64decode
+from hashlib import md5
 
-from Crypto.Cipher import AES
-from Crypto import Random
 import psycopg2
 from pygments import highlight, util
 from pygments.lexers import get_lexer_by_name, TextLexer
@@ -14,36 +11,6 @@ from PyPaste.models import BaseModel
 
 
 class Paste(BaseModel):
-
-    @staticmethod
-    def _encrypt(key, plaintext):
-        if isinstance(plaintext, unicode):
-            # Convert to ascii
-            plaintext = plaintext.encode('utf8')
-
-        iv = Random.new().read(AES.block_size)
-        key = sha256(key).digest()
-        cipher = AES.new(key, AES.MODE_CFB, iv)
-        ciphertext = iv + cipher.encrypt(plaintext)
-
-        # b64 and back to unicode for storage
-        ciphertext = b64encode(ciphertext)
-        ciphertext = ciphertext.decode('utf8')
-        return ciphertext
-
-    @staticmethod
-    def _decrypt(key, ciphertext):
-        # Decode b64, also converts to str
-        # Later converted back to unicode
-        ciphertext = b64decode(ciphertext)
-
-        key = sha256(key).digest()
-        iv = ciphertext[:AES.block_size]
-        cipher = AES.new(key, AES.MODE_CFB, iv)
-        plaintext = cipher.decrypt(ciphertext)[AES.block_size:]
-        plaintext = plaintext.decode('utf8')
-
-        return plaintext
 
     @staticmethod
     def _highlight(text, language='text'):
@@ -84,9 +51,6 @@ class Paste(BaseModel):
 
     @classmethod
     def new(self, text, title=None, language='text', password=None, unlisted=False):
-        if isinstance(password, unicode):
-            password = password.encode('utf8')
-
         if title is None:
             title = 'Untitled'
 
@@ -96,11 +60,6 @@ class Paste(BaseModel):
             unlisted = False
 
         if password is not None:
-            # Encrypt input text and hash password
-            # Plaintext password is used as the key,
-            # and bcrypt hash is stored
-            text = self._encrypt(password, text)
-            highlighted = self._encrypt(password, highlighted)
             password = self._hash_password(password)
 
         _hash = md5(urandom(64)).hexdigest()
